262a4911193cd93997b2b4ed9fa2b5d8707b4371
Pey <peynuri@yahoo.com>
2018-11-09 11:43:47 -0800

    get last day of last month method added

--- src/helper/CalendarHelper.java
+++ src/helper/CalendarHelper.java
@@ -7,20 +7,21 @@ import java.util.Calendar;
 
 public class CalendarHelper {
 
-    public static int getMonthFromString(String month) throws ParseException {
+    public static int getMonthFromString(String month) {
         Calendar calendar = Calendar.getInstance();
-        Date date = new Date(new SimpleDateFormat("MMMM").parse(month).getTime());
+        Date date = null;
+        try {
+            date = new Date(new SimpleDateFormat("MMMM").parse(month).getTime());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         calendar.setTime(date);
         return calendar.get(Calendar.MONTH) + 1;
     }
 
     public static int getFirstDayOfWeek(String month, int year) {
         int monthNum = 0;
-        try {
-            monthNum = getMonthFromString(month);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        monthNum = getMonthFromString(month);
         Calendar calendar = Calendar.getInstance();
         Date date = null;
         try {
@@ -33,12 +34,20 @@ public class CalendarHelper {
     }
 
     public static int getLastDayOfMonth(String month, int year) {
-        int monthNum = 0;
+        int monthNum = getMonthFromString(month);
+        Calendar calendar = Calendar.getInstance();
+        Date date = null;
         try {
-            monthNum = getMonthFromString(month);
+            date = new Date(new SimpleDateFormat("yyyy-M-dd").parse(Integer.toString(year) + "-" + Integer.toString(monthNum) + "-01").getTime());
         } catch (ParseException e) {
             e.printStackTrace();
         }
+        calendar.setTime(date);
+        return calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
+    }
+
+    public static int getLastDayOfLastMonth(String month, int year) {
+        int monthNum = getMonthFromString(month) - 1;
         Calendar calendar = Calendar.getInstance();
         Date date = null;
         try {
