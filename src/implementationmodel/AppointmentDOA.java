966137e8f2fe4d94bfa74d273f1b5df3373d5e1e
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 13:45:34 -0800

    test

--- src/implementationmodel/AppointmentDOA.java
+++ src/implementationmodel/AppointmentDOA.java
@@ -1,10 +1,65 @@
 package implementationmodel;
 
-<<<<<<< HEAD
 import interfacemodel.DelegationOfAuthority;
 
+import java.util.List;
+
 public class AppointmentDOA implements DelegationOfAuthority {
-=======
+
+    
 public class AppointmentDOA {
->>>>>>> parent of 92a1c88... work on appointmentdoa next time
+
+}
+    /**
+     * Creates Table
+     */
+    @Override
+    public void CreateTable() {
+        
+    }
+
+    /**
+     * Deletes Table Rows
+     */
+    @Override
+    public void DropTable() {
+
+    }
+
+    /**
+     * Insert Row Into Table
+     *
+     * @param t Row To Insert
+     */
+    @Override
+    public void Insert(Object t) {
+
+    }
+
+    /**
+     * Update Row In Table
+     *
+     * @param o Row To Update
+     */
+    @Override
+    public void Update(Object o) {
+
+    }
+
+    /**
+     * @return List of All the Rows In Table
+     */
+    @Override
+    public List GetList() {
+        return null;
+    }
+
+    /**
+     * @param o row to check in table
+     * @return true if exist false if doesn't
+     */
+    @Override
+    public boolean Exists(Object o) {
+        return false;
+    }
 }
