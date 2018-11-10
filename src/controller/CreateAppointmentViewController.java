8636e740de9d346b07f18f8bb45bb8de22a6464d
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 12:59:14 -0800

    added view fields to class

--- src/controller/CreateAppointmentViewController.java
+++ src/controller/CreateAppointmentViewController.java
@@ -1,9 +1,32 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
+/**
+ * Responsible for appointment creation view
+ */
 public class CreateAppointmentViewController {
-    
+
+    @FXML
+    private TextField username;
+
+    @FXML
+    private TextField event;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private ComboBox time;
+
+    /**
+     * Adds appointment to database corresponding to current user
+     * @param event
+     */
     public void createAppointmentBtn(ActionEvent event) {
 
     }
