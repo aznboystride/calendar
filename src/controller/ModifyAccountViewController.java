3313c6d13a6f17db3063020e839e60695e149c78
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 12:19:38 -0800

    added javadocs

--- src/controller/ModifyAccountViewController.java
+++ src/controller/ModifyAccountViewController.java
@@ -2,8 +2,15 @@ package controller;
 
 import javafx.event.ActionEvent;
 
+/**
+ * This controller is associated with the modifyaccount view
+ */
 public class ModifyAccountViewController extends Controller {
 
+    /**
+     * When button is clicked, the Change Password View will load
+     * @param event ActionEvent associated with the button click
+     */
     public void changePasswordBtnPressed(ActionEvent event) {
         LoadFXML(event, "Calendar App - Change Password", "/fxml/ChangePasswordView.fxml");
     }
