1ab3e540f48b9263eb6f9bbf9d7ddc1c99a82e7a
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 12:37:35 -0800

    added javadocs

--- src/interfacemodel/DBConnectionManager.java
+++ src/interfacemodel/DBConnectionManager.java
@@ -4,7 +4,14 @@ import java.sql.Connection;
 
 public interface DBConnectionManager {
 
+    /**
+     * Determines how database connection is established
+     * @return Connection to database
+     */
     Connection GetConnection();
 
+    /**
+     * Closes the current connection to database
+     */
     void CloseConnection();
 }
--- src/interfacemodel/DelegationOfAuthority.java
+++ src/interfacemodel/DelegationOfAuthority.java
@@ -4,16 +4,39 @@ import java.util.List;
 
 public interface DelegationOfAuthority<Type> {
 
+    /**
+     * Creates Table
+     */
     void CreateTable();
 
+    /**
+     * Deletes Table Rows
+     */
     void DropTable();
 
+    /**
+     * Insert Row Into Table
+     * @param t Row To Insert
+     */
     void Insert(Type t);
 
+    /**
+     * Update Row In Table
+     * @param o Row To Update
+     */
     void Update(Type o);
 
+    /**
+     *
+     * @return List of All the Rows In Table
+     */
     List<Type> GetList();
 
+    /**
+     *
+     * @param o row to check in table
+     * @return true if exist false if doesn't
+     */
     boolean Exists(Type o);
 
 }
