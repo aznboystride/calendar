c15cdfaf8561a1bbd4c5b3095662c9bdd975cf9a
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 13:43:16 -0800

    test

--- src/implementationmodel/AppointmentDOA.java
+++ src/implementationmodel/AppointmentDOA.java
@@ -1,6 +1,10 @@
 package implementationmodel;
 
+<<<<<<< HEAD
 import interfacemodel.DelegationOfAuthority;
 
 public class AppointmentDOA implements DelegationOfAuthority {
+=======
+public class AppointmentDOA {
+>>>>>>> parent of 92a1c88... work on appointmentdoa next time
 }
