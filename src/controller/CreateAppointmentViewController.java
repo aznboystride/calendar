2fadce635b6d8144f844ef672784bb3637f1f1c2
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-10 13:06:39 -0800

    extends Controller implements Initializable

--- src/controller/CreateAppointmentViewController.java
+++ src/controller/CreateAppointmentViewController.java
@@ -2,14 +2,18 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Responsible for appointment creation view
  */
-public class CreateAppointmentViewController {
+public class CreateAppointmentViewController extends Controller implements Initializable {
 
     @FXML
     private TextField username;
@@ -29,5 +33,19 @@ public class CreateAppointmentViewController {
      */
     public void createAppointmentBtn(ActionEvent event) {
 
+    }
+
+
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
     }
 }
