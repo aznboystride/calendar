5952769c54342cfcf531f1d041313a6bd5961716
Pey <peynuri@yahoo.com>
2018-11-09 23:05:01 -0800

    Made class static to save heap space

--- src/helper/CalendarPoint.java
+++ src/helper/CalendarPoint.java
@@ -5,19 +5,20 @@ import javafx.scene.layout.GridPane;
 
 public class CalendarPoint {
 
-    private int x;
-    private int y;
+    private static int x;
+    private static int y;
 
-    public CalendarPoint(Node node) {
+    public static void setPoint(Node node) {
         x = GridPane.getColumnIndex(node);
         y = GridPane.getRowIndex(node);
     }
 
-    public int GetDayNumber() {
+
+    private static int GetDayNumber() {
         return 1 + x + y * 7;
     }
 
-    public int compare(int num) {
+    public static int compare(int num) {
         return GetDayNumber() == num ? 0 : GetDayNumber() < num ? -1 : 1;
     }
 }
