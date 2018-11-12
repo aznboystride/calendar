df8f315e5157ebc3d6b7d06866c028e4a6131e9b
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-12 02:18:31 -0800

    created weeklyviewcontroller and set up weeklyview.fxml

---
+++ src/controller/WeeklyViewController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+
+public class WeeklyViewController {
+
+    public void signOffBtn(ActionEvent event) {
+
+    }
+
+    public void weekBtn(ActionEvent event) {
+
+    }
+
+    public void setCalendarColor(ActionEvent event) {
+
+    }
+
+    public void keyTypedMonth(KeyEvent keyEvent) {
+
+    }
+
+    public void monthBtn(ActionEvent event) {
+
+    }
+
+    public void yearBtn(ActionEvent event) {
+
+    }
+}
