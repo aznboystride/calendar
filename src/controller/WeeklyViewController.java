4d4252d6e19f5a6cea9be9fa77b7546d4b948643
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-11-12 04:34:32 -0800

    completely fixed set calendar range for weeklyview

--- src/controller/WeeklyViewController.java
+++ src/controller/WeeklyViewController.java
@@ -5,6 +5,7 @@ import helper.CalendarPoint;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -18,13 +19,19 @@ public class WeeklyViewController extends CalendarViewController {
     @FXML
     private GridPane gridPane;
 
+    @FXML
+    private ComboBox weekCombo;
+
+    /**
+     * Initializes weekly view gridpane
+     */
     @Override
     protected void initializeGridPaneCells() {
         Label l;
         Pane p;
         for(int col = 0; col < 7; col++) {
             for(int row = 0; row < 5; row++) {
-                l = getCalendarLabel();
+                l = getCalendarLabel(80, 5);
                 p = getCalendarPane(l);
                 gridPane.add(p, col, row);
             }
@@ -55,34 +62,56 @@ public class WeeklyViewController extends CalendarViewController {
         initializeMonthDropDown();
         initializeYearDropDown();
         initializeGridPaneCells();
-        setCalendarRange(month.getPromptText(), Integer.valueOf(year.getPromptText()));
+        initializeWeekCombo();
+        setCalendarRange(month.getPromptText(), Integer.valueOf(year.getPromptText()), Integer.parseInt(Character.toString(weekCombo.getPromptText().charAt(5))));
         setGridPaneConstraints();
     }
 
     public void weekBtn(ActionEvent event) {
-
+        setCalendarRange(monthLabel.getText(), Integer.parseInt(year.getPromptText()), Integer.parseInt(Character.toString(weekCombo.getValue().toString().charAt(5))));
     }
 
-    @Override
-    protected void setCalendarRange(String month, int year) {
+    /**
+     * Sets calendar range of first week
+     * @param month String month associated with calendar view
+     * @param year int year associated with calendar view
+     * @param week int week associated with weekly view
+     */
+    protected void setCalendarRange(String month, int year, int week) {
         clearCalendarRange(gridPane);
         int lastDayOfMonth = CalendarHelper.getLastDayOfMonth(month, year);
+        int lastDayOfLastMonth = CalendarHelper.getLastDayOfLastMonth(month, year);
         int firstDayOfWeek = CalendarHelper.getFirstDayOfWeek(month, year);
         for (Node node : gridPane.getChildren()) {
             if (node instanceof Pane) {
                 for(Node p : ((Pane) node).getChildren()) {
                     if (p instanceof Label) {
                         CalendarPoint.setPoint(node);
+                        CalendarPoint.setY(0);
                         if(CalendarPoint.compare(firstDayOfWeek) < 0 && GridPane.getRowIndex(node) == 0) {
-
-                        } else if(CalendarPoint.compare(lastDayOfMonth + firstDayOfWeek - 1) > 0) {
-
+                            CalendarPoint.setPoint(node);
+                            ((Label) p).setText(String.valueOf(((1 + CalendarPoint.GetDayNumber() + lastDayOfLastMonth - firstDayOfWeek + 7 * (week-1)) % lastDayOfLastMonth) == 0 ? lastDayOfLastMonth : ((1 + CalendarPoint.GetDayNumber() + lastDayOfLastMonth - firstDayOfWeek + 7 * (week-1)) % lastDayOfLastMonth)));
+                            ((Label) p).setStyle("-fx-text-fill: gray");
+                        } else if(CalendarPoint.compare(lastDayOfMonth + firstDayOfWeek - 1) > 0 && GridPane.getRowIndex(node) == 0) {
+                            CalendarPoint.setPoint(node);
+                            ((Label) p).setText(String.valueOf((week-1) * 7 + CalendarPoint.GetDayNumber() - lastDayOfMonth - firstDayOfWeek + 1));
+                            ((Label) p).setStyle("-fx-text-fill: gray");
                         } else if(GridPane.getRowIndex(node) == 0){
-                            ((Label) p).setText(String.valueOf(1 + CalendarPoint.GetDayNumber() - firstDayOfWeek));
+                            CalendarPoint.setPoint(node);
+                            ((Label) p).setText(String.valueOf((week-1) * 7 + 1 + CalendarPoint.GetDayNumber() - firstDayOfWeek));
                         }
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * Initializes week combo box
+     */
+    private void initializeWeekCombo() {
+        for(short i = 0; i < 4; i++) {
+            weekCombo.getItems().add("Week " + (i+1));
         }
     }
 }
