0677c325934fa2d471c33ee45792ff90089d0ed1
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2018-12-04 17:37:31 -0800

    added thread that checks for reminders

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 
+import helper.DateTimeParser;
+import helper.Email;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import object.Appointment;
+import object.User;
+
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -20,7 +28,46 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-
+        Thread thread = new Thread() {
+            public void run() {
+                SettleReminders();
+            }
+        };
+        thread.start();
         launch(args);
+    }
+
+    public static void SettleReminders() {
+        List<Appointment> alreadyEmailed = new ArrayList<>();
+        while(true) {
+            List<Appointment> appointments = User.GetInstance().getAppointments();
+            String username = User.GetInstance().getUserName();
+            String from, pass, to, sub, body;
+            from = null;
+            pass = null;
+            to = null;
+            sub = null;
+            body = null;
+            try {
+                for (Appointment app : appointments) {
+                    if (alreadyEmailed.contains(app))
+                        continue;
+                    if (app.getUsername().equals(username)) {
+                        if (app.getTime().getTime() - System.currentTimeMillis() < app.getReminder().getTime()) {
+                            from = "peynuri@yahoo.com";
+                            to = User.GetInstance().getEmail();
+                            pass = "S6flipto!@#";
+                            sub = "Appointment Reminder With: " + app.getWithperson();
+                            body = app.getDate() + " @ " + DateTimeParser.getHourMinFromDate(app.getTime());
+                            Email.sendEmail(from, pass, to, sub, body);
+                            alreadyEmailed.add(app);
+                            System.out.println("Emailed Appointment\n" + app);
+                        }
+                    }
+                }
+            } catch(ConcurrentModificationException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
